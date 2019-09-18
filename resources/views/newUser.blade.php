b361a56fc03e6ff3f89d6c14bec852abadadf793
Leandro Bellizzi <lbellizzi.dev@gmail.com>
2019-09-18 13:44:30 -0300

    bug fixed

--- resources/views/newUser.blade.php
+++ resources/views/newUser.blade.php
@@ -30,7 +30,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Sobrenome') }}</label>
 
                             <div class="col-md-6"> 
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
